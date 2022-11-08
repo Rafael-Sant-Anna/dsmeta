c45cfc9946117480da2647e6b233a0ed25b37294
Rafael-Sant-Anna <santanna.fael@gmail.com>
2022-11-08 06:38:30 -0300

    Database

---
+++ backend/src/main/java/com/santanna/dsmeta/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.santanna.dsmeta.config;
+
+
+
+
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) values ('Barry Allen', 121,67,18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) values ('Logan', 26,14,4255.0,'2022-06-14');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) values ('Padme', 55,42,13249.0,'2022-06-14');
