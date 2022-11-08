0848f639ebd44b10006c36ac32d3d6e2dea978c8
Rafael-Sant-Anna <santanna.fael@gmail.com>
2022-11-08 19:37:57 -0300

    API test

---
+++ backend/src/main/java/com/santanna/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.santanna.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.santanna.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
