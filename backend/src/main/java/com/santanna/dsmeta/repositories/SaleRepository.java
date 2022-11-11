c741ca5c33de03dad30b9d9ab0de23a74baa4281
Rafael-Sant-Anna <santanna.fael@gmail.com>
2022-11-10 21:23:32 -0300

    Date Select

--- backend/src/main/java/com/santanna/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/santanna/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,16 @@
 package com.santanna.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.santanna.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	@Query("SELECT obj FROM Sale obj where obj.date BETWEEN :DtInicio and :DtFim ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate DtInicio, LocalDate DtFim, Pageable pageable);
 
 }
--- backend/src/main/java/com/santanna/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/santanna/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.santanna.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.santanna.dsmeta.entities.Sale;
@@ -14,9 +18,13 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() 
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) 
 	{
-		return repository.findAll();
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate DtInicio = minDate.equals("")? today.minusDays(365): LocalDate.parse(maxDate);
+		LocalDate DtFim = maxDate.equals("")? today: LocalDate.parse(maxDate);
+		
+		return repository.findSales(DtInicio,DtFim,pageable);
 	}
 
 }
